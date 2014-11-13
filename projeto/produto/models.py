6c42005dd7573d60da3c510e3ebeb5a2764e501b
Thiago22 <thiago5046@hotmail.com>
2014-11-13 23:26:27 +0000

    Testando classe produto

--- projeto/produto/models.py
+++ projeto/produto/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    produto = models.CharField(max_length=100)
+    fornecedor = models.CharField(max_length=70)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
