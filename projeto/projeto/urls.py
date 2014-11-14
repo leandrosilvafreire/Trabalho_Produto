905bc83696e67fa2b1a0a76ce0eb58e1f611bf5c
leandrosilvafreire <leandrofreire3@hotmail.com>
2014-11-14 23:18:34 +0000

    Qinto commit

--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -3,12 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
    url(r'^$', 'produto.views.index'),
    url(r'^validar/$', 'produto.views.validar'),
-   url(r'^validar/$', 'produto.views.validar'),
-   url(r'^validar/$', 'produto.views.validar'),
-   url(r'^validar/$', 'produto.views.validar'),
-   url(r'^validar/$', 'produto.views.validar'),
-   url(r'^validar/$', 'produto.views.validar'),
-   url(r'^validar/$', 'produto.views.validar'),
-   url(r'^validar/$', 'produto.views.validar'),
-
+   
 )
