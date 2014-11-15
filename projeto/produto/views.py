3c76b633dc43d187ef58d21847810e630359fd93
leandrosilvafreire <leandrofreire3@hotmail.com>
2014-11-15 17:41:23 +0000

    Decimo commit

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -7,4 +7,11 @@ def index(request):
 
 def validar(request):
     if request.method == 'POST':
-        form = ProdutoForm(request.POST)
+        form = ProdutoForm(request.POST) 
+        
+        if form.is_valid():
+            produto = Produto(**form.cleaned_data)
+            produto.save()
+        
+        
+        
