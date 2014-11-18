010d24b175eb98c4dd1fe259b8989dd03facde8a
leandrosilvafreire <leandrofreire3@hotmail.com>
2014-11-18 23:44:36 +0000

    Teste

--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -1,19 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-<<<<<<< HEAD
-urlpatterns = patterns('',
-   
-   url(r'^$', 'produto.views.index'),
-   url(r'^cadastro/$', 'produto.views.cadastro'),
-   url(r'^validar/$', 'produto.views.validar'),
-   
-   
-=======
 urlpatterns = patterns('produto.views',
                        url(r'^$', 'index'),
                        url(r'^cadastro/$', 'cadastro'),
                        url(r'^validar/$', 'validar'),
->>>>>>> b3bce85639d1dd43fa17fd0dbb238676a51d75b5
                        
                        url(r'^i18n/', include('django.conf.urls.i18n')),
-)
+                      )
